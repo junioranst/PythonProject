8f274d3a079fc4bac4625bd1fa553270be4395e5
junioranst <junioranmms@gmail.com>
2026-04-04 06:56:12 -0300

    version01

---
+++ NovoExercicios/ex001.py
@@ -0,0 +1,46 @@
+vendas = [
+    {"produto": "Notebook", "preco": 3500, "quantidade": 2},
+    {"produto": "Mouse", "preco": 80, "quantidade": 10},
+    {"produto": "Teclado", "preco": 150, "quantidade": 5},
+    {"produto": "Monitor", "preco": 900, "quantidade": 3}
+]
+
+def calcular_faturamento(vendas):
+    total = 0
+    for venda in vendas:
+        total += venda['preco'] * venda['quantidade']
+    return total
+
+def produto_mais_vendido(vendas):
+    mais_vendido = 0
+    nome_mais_vendido = ''
+    for venda in vendas:
+        if venda['quantidade'] > mais_vendido:
+            mais_vendido = venda['quantidade']
+            nome_mais_vendido = venda['produto']
+    return nome_mais_vendido
+
+def produto_maior_receita(vendas):
+    maior_receita = 0
+    nome_maior_receita = ''
+    for venda in vendas:
+        if venda['preco'] * venda['quantidade'] > maior_receita:
+            maior_receita = venda['preco'] * venda['quantidade']
+            nome_maior_receita = venda['produto']
+    return nome_maior_receita, maior_receita
+
+valor_minimo = 1000
+def produtos_caros(vendas, valor_minimo):
+    lista_caros = []
+    for venda in vendas:
+        if venda['preco'] > valor_minimo:
+            lista_caros.append(venda['produto'])
+    return lista_caros
+
+def main():
+    print(f'O faturamento total foi de: {calcular_faturamento(vendas)}')
+    print(f'O produto mais vendido foi: {produto_mais_vendido(vendas)}')
+    print(f'O produto que gerou mais receita foi: {produto_maior_receita(vendas)}')
+    print(f'Os Produtos acima de R$1.000 sao: {produtos_caros(vendas, valor_minimo)}')
+
+main()
