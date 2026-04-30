e4eb9b9b372d8ca6abe6390c21b6319d5d3d743c
junioranst <junioranmms@gmail.com>
2026-04-29 23:32:38 -0300

    v4

---
+++ ProjectPOO/ex011/__main__.py
@@ -0,0 +1,11 @@
+from ex011 import *
+
+
+a1 = Aluno('joao', 22, 'matematica,', '002')
+print(a1)
+
+p1 = Professor('Romero', 43, 'Matemática', 'Mestre')
+inspect(p1)
+p1.aniversario()
+p1.darAula()
+print(p1)
